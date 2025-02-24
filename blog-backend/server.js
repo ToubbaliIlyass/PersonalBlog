84e7966e5561786329a26b08d560cdeb85cac7c5
Ilyass Toubbali <toubbaliilyasse@gmail.com>
2025-02-24 19:33:40 +0000

    again

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -1,69 +1,4 @@
-// const express = require("express");
-// const { Pool } = require("pg");
 const cors = require("cors");
-// require("dotenv").config();
-// const { createClient } = require('@supabase/supabase-js');
-
-// // Initialize Supabase client
-// const supabase = createClient(
-//   process.env.SUPABASE_URL,
-//   process.env.SUPABASE_API_KEY
-// );
-
-// const app = express();
-
-// // Middleware
-// // app.use(cors());
-
-// app.use(express.json());
-// // Database configuration
-
-// // Routes
-// app.get("/api/blogs", async (req, res) => {
-//   try {
-//     console.log("called /api/blogs");
-//     const { rows } = await pool.query(
-//       "SELECT * FROM public.blogs ORDER BY created_at DESC"
-//     );
-//     res.json(rows);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// app.get("/api/blogs/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { rows } = await pool.query(
-//       "SELECT * FROM public.blogs WHERE id = $1",
-//       [id]
-//     );
-//     if (rows.length === 0) {
-//       return res.status(404).json({ error: "Blog not found" });
-//     }
-//     res.json(rows[0]);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// app.post("/api/blogs", async (req, res) => {
-//   try {
-//     const { title, content, slug } = req.body;
-//     const { rows } = await pool.query(
-//       "INSERT INTO public.blogs (title, content, slug) VALUES ($1, $2, $3) RETURNING *",
-//       [title, content, slug]
-//     );
-//     res.status(201).json(rows[0]);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// const PORT = process.env.PORT || 5001;
-// app.listen(PORT, async () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 require("dotenv").config();
 const express = require("express");
@@ -73,7 +8,7 @@ app.use(express.json()); // To parse JSON requests
 
 app.use(
   cors({
-    origin: "https://personal-blog-beryl-seven.vercel.app/#/", // Change this to your frontend URL
+    origin: "https://personal-blog-beryl-seven.vercel.app", // Change this to your frontend URL
   })
 );
 // Initialize Supabase client
