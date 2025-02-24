d9eb2a385f490a120a5c3d0c66b5631aa6610c38
Ilyass Toubbali <toubbaliilyasse@gmail.com>
2025-02-24 20:07:13 +0000

    added api in the adresses of the server services

--- Blog/src/services/api.ts
+++ Blog/src/services/api.ts
@@ -4,17 +4,17 @@ const API_URL = 'https://personalblog-4kkc.onrender.com';
 
 export const api = {
   getAllBlogs: async () => {
-    const response = await axios.get(`${API_URL}/blogs`);
+    const response = await axios.get(`${API_URL}/api/blogs`);
     return response.data;
   },
 
   getBlogById: async (id: string) => {
-    const response = await axios.get(`${API_URL}/blogs/${id}`);
+    const response = await axios.get(`${API_URL}/api/blogs/${id}`);
     return response.data;
   },
 
   createBlog: async (blogData: { title: string; content: string; slug: string }) => {
-    const response = await axios.post(`${API_URL}/blogs`, blogData);
+    const response = await axios.post(`${API_URL}/api/blogs`, blogData);
     return response.data;
   }
 };
